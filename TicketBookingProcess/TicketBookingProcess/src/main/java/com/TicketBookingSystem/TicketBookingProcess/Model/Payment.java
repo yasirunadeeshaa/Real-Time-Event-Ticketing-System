b9871d1969e311f8dcc360eff0aff415ad347338
A.YASIRU NADEESHA ATHTHANAYAKA <yasiru.20232389@iit.ac.lk>
2024-11-19 15:03:06 +0530

    init commit

--- TicketBookingProcess/TicketBookingProcess/src/main/java/com/TicketBookingSystem/TicketBookingProcess/Model/Payment.java
+++ TicketBookingProcess/TicketBookingProcess/src/main/java/com/TicketBookingSystem/TicketBookingProcess/Model/Payment.java
@@ -2,4 +2,8 @@ package com.TicketBookingSystem.TicketBookingProcess.Model;
 
 public class Payment {
     private int id;
+    private String paymentDate;
+    private String paymentType;
+    private String paymentStatus;
+
 }
